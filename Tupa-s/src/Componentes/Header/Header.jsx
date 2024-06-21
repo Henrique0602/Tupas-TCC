6cdaf76df7d75dbab5da3657875a4338ab8e10a0
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-21 09:08:06 -0300

    ola

--- Tupa-s/src/Componentes/Header/Header.jsx
+++ Tupa-s/src/Componentes/Header/Header.jsx
@@ -1,10 +1,14 @@
 import { FaRegUserCircle } from "react-icons/fa";
 
+
 function Header(){
     return (
+      
       <div className=" flex justify-between items-center bg-color_bg text-color_vinho p-5  ">
         <img className="w-16 ml-10" src="../Public/Logo.png" alt="" />
         <FaRegUserCircle className="text-3xl mr-3" />
+        
+        
       </div>
     );
 }
--- Tupa-s/src/Pages/Home.jsx
+++ Tupa-s/src/Pages/Home.jsx
@@ -6,8 +6,8 @@ function Home() {
     return (
         <div className="min-h-screen bg-Fundo bg-cover">
             <Header />
-            <div className="p-8 w-full h-full flex flex-row bg-color_bg bg-opacity-75 mb-10">
-                <div className="flex-col text-color_vinho ml-12 mt-6">
+            <div className="p-20 w-full h-full flex flex-row bg-color_bg bg-opacity-75 ">
+                <div className="flex-col text-color_vinho ml-12 mt-6 mb-10">
 
                     <h2 className=" font-bold text-xl mb-10">Home</h2>
                     <div className="flex-col ">
@@ -18,7 +18,7 @@ function Home() {
                 </div>
             </div>
 
-            <h1 className="text-xl text-colortxt_white font-bold px-8 ml-10 mb-6">Serviços</h1>
+            <h1 className="text-xl text-color text-color_bg font-bold px-20 mt-24 ml-10 mb-40 ">Serviços</h1>
             <div className="flex flex-row px-8 justify-around mb-6">
 
                 <div className="flex flex-col items-center">
@@ -37,12 +37,19 @@ function Home() {
                 </div>
 
             </div>
-            
-            <div className="w-full flex flex-row justify-center">
-                <button type="submit" className="bg-color_vinho text-colortxt_white py-2 rounded-xl w-60 mb-2 text-center hover:bg-color_bege hover:text-color_vinho font-semibold">Saber Mais</button>
+
+            <div className="w-full flex flex-row justify-center mt-20 mb-20">
+                <button type="submit" className="bg-color_bg text-color text-color_vinho py-2 rounded-xl w-60 mb-2 text-center font-semibold">Saber Mais</button>
             </div>
-            
-        </div>       
+
+            <div className="p-8 w-full h-full flex flex-row bg-color_bg bg-opacity-75 mb-10">
+                <h2 className=" font-bold text-xl text-color_vinho mb-10">Planos Exclusivos</h2>
+                <div className="">
+                   
+                </div>
+            </div>
+
+        </div>
     )
 }
 export default Home
