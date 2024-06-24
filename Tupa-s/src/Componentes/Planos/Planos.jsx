9538b1d9294501c23f896fe248500ec7603a191e
Henrique0602 <henriquef.brigo@gmail.com>
2024-06-23 21:30:01 -0300

    Telas

--- Tupa-s/src/Componentes/Planos/Planos.jsx
+++ Tupa-s/src/Componentes/Planos/Planos.jsx
@@ -1,45 +1,170 @@
 import React from 'react';
-import CardPlano from '../CardPlano/CardPlano';
 
-const Planos = () => {
-  const beneficiosBasico = ["Benefício 1", "Benefício 2", "Benefício 3"];
-  const beneficiosPremium = ["Benefício 1", "Benefício 2", "Benefício 3", "Benefício 4", "Benefício 5"];
 
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="w-full max-w-6xl p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl text-vinho font-bold text-center mb-12 text-color_vinho">
+  function Planos (){
+    return (
+      <div className="card-container bg-color_bg bg-opacity-75  shadow-md  ">
+      <h1 className="text-2xl text-color text-color_vinho font-bold pt-10 mt-24 ml-10">
           Planos Exclusivos
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <CardPlano
-            title="Gratuito - Homem das Cavernas"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis minus fuga magnam ipsum quae saepe."
-            price="00,00R$"
-            benefits={["Benefício 1", "Benefício 2", "Benefício 3", "Benefício 4"]}
-          />
-          <CardPlano
-            title="Básico - Homem Moderno"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis minus fuga magnam ipsum quae saepe."
-            price="99,99R$"
-            benefits={beneficiosBasico}
-          />
-          <CardPlano
-            title="Premium - Homem a Frente do seu Tempo"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis minus fuga magnam ipsum quae saepe."
-            price="199,99R$"
-            benefits={beneficiosPremium}
-          />
-          <CardPlano
-          title="Penis Enorme"
-          description="Teu pai leva penis"
-          price="00,00R$"
-          benefits={beneficiosPremium}
-          />
-        </div>
+      </h1>
+     
+          <div className="flex flex-col md:flex-row gap-8 p-10 justify-center items-center">
+              {/* Gratis */}
+              <div className="bg-color_vinho text-white rounded-lg shadow-lg p-10 flex flex-col justify-between skew-y-6 hover:scale-110 transition-transform duration-300 ease-in-out">
+                  <div>
+                      <h3 className="text-xl font-semibold mb-4">Gratis</h3>
+                      <p className="text-4xl font-bold">
+                          R$ 00,00
+                          <span className="text-base font-medium">/mês</span>
+                      </p>
+                      <p className="mt-4 mb-8">
+                          O plano gratis da Tupã's, você tem direito ao mínimos recursos
+                      </p>
+                      <ul className="mb-4">
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-red-500 rounded-full mr-2"></span>
+                            
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-red-500 rounded-full mr-2"></span>
+                             
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-red-500 rounded-full mr-2"></span>
+                          
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-red-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-red-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-red-500 rounded-full mr-2"></span>
+                             
+                          </li>
+                      </ul>
+                  </div>
+                  <button className="mt-6 py-2 px-4 bg-color_bg rounded text-color_vinho font-bold">
+                      Selecionar Plano
+                  </button>
+              </div>
+  
+              {/* TRIMESTRAL */}
+              <div className="bg-color_vinho text-white rounded-lg shadow-lg p-8 flex flex-col justify-between skew-y-6 hover:scale-110 transition-transform duration-300 ease-in-out ">
+                  <div>
+                      <h3 className="text-xl font-semibold mb-4">TRIMESTRAL</h3>
+                      <p className="text-4xl font-bold">
+                          R$ 49,00
+                          <span className="text-base font-medium">/mês</span>
+                      </p>
+                      <p className="mt-4 mb-8">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      </p>
+                      <ul className="mb-4">
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                           
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                           
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                            
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                            
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                             
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                             
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-red-500 rounded-full mr-2"></span>
+                            
+                          </li>
+                      </ul>
+                  </div>
+                  <button className="mt-6 py-2 px-4 bg-color_bg rounded text-color_vinho font-bold">
+                      Selecionar Plano
+                  </button>
+              </div>
+  
+               {/* Anual */}
+               <div className="bg-color_vinho text-white rounded-lg shadow-lg p-8 flex flex-col justify-between skew-y-6 hover:scale-110 transition-transform duration-300 ease-in-out">
+                  <div>
+                      <h3 className="text-xl font-semibold mb-4">Anual</h3>
+                      <p className="text-4xl font-bold">
+                          R$ 150,00
+                          <span className="text-base font-medium">/mês</span>
+                      </p>
+                      <p className="mt-4 mb-8">
+                          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                      </p>
+                      <ul className="mb-4">
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                           
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                             
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                             
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                             
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                          <li className="flex items-center mb-2">
+                              <span className="w-2 h-2 bg-green-500 rounded-full mr-2"></span>
+                              
+                          </li>
+                      </ul>
+                  </div>
+                  <button className="mt-6 py-2 px-4 bg-color_bg rounded text-color_vinho font-bold">
+                      Selecionar Plano
+                  </button>
+              </div>
+          </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+  
 
 export default Planos;
--- Tupa-s/src/Pages/Home.jsx
+++ Tupa-s/src/Pages/Home.jsx
@@ -2,57 +2,113 @@ import { Link } from "react-router-dom"
 import Header from "../Componentes/Header/Header";
 import Planos from "../Componentes/Planos/Planos";
 
+
 function Home() {
     return (
-     
-            <div className="min-h-screen bg-Fundo bg-cover">
-                <Header />
-                <div className="p-20 w-full h-full flex flex-row bg-color_bg bg-opacity-75 ">
-                    <div className="flex-col text-color_vinho ml-12 mt-6 mb-10">
+        <div className="min-h-screen bg-Fundo bg-cover">
+            <Header />
+            <div className="p-20 w-full h-full flex flex-row bg-color_bg bg-opacity-75 ">
+                <div className="flex-col text-color_vinho ml-12 mt-6 mb-10">
+                    <h2 className=" font-bold text-2xl mb-10">Home</h2>
+                    <div className="flex-col ">
+                        <h1 className="text-2xl font-semibold text-wrap w-80 mb-2">
+                            Seu guia para o sucesso físico e mental.
+                        </h1>
+                        <p className="text-wrap w-80 text-sm">
+                            Lorem ipsum dolor sit amet consectetur. Id luctus a a sed lectus
+                            aenean aenean tortor. Ultrices felis sed donec est urna aliquet.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-                        <h2 className=" font-bold text-2xl mb-10">Home</h2>
-                        <div className="flex-col ">
-                            <h1 className="text-2xl font-semibold text-wrap w-80 mb-2">Seu guia para o sucesso físico e mental.</h1>
-                            <p className="text-wrap w-80 text-sm">Lorem ipsum dolor sit amet consectetur. Id luctus a a sed lectus aenean aenean tortor. Ultrices felis sed donec est urna aliquet.</p>
+            <h1 className="text-2xl text-color text-color_bg font-bold px-20 mt-24 ml-10 mb-32">
+                Serviços
+            </h1>
+            <div className=" flex flex-col md:flex-row gap-20 p-10 justify-center items-center ">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-colortxt_white font-semibold mb-2">
+                        Estética Física
+                    </h1>
+                    <img className="w-56 rounded-xl mt-5 skew-y-6 hover:scale-110 transition-transform duration-300 ease-in-out" src="/Public/Físico.jpg" />
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <h1 className="text-colortxt_white font-semibold mb-2">
+                        Saúde Corporal
+                    </h1>
+                    <img className="w-56 rounded-xl mt-5 skew-y-6 hover:scale-110 transition-transform duration-300 ease-in-out" src="/Public/SaudeCorporal.jpg" />
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <h1 className="text-colortxt_white font-semibold mb-2">
+                        Saúde Mental
+                    </h1>
+                    <img className="w-56 rounded-xl mt-5 skew-y-6 hover:scale-110 transition-transform duration-300 ease-in-out" src="/Public/Mente.jpg" />
+                </div>
+            </div>
+
+            <div className="w-full flex flex-row justify-center mt-28 mb-24">
+                <button
+                    type="submit"
+                    className="bg-color_bg text-color text-color_vinho py-2 rounded-xl w-60 mb-2 text-center font-semibold">
+                    Saber Mais
+                </button>
+            </div>
+
+            <Planos />
+
+            <div className="px-4 md:px-10 ">
+                <h1 className="text-2xl font-bold text-color_bg p-10 ml-4">Consultas Online</h1>
+        
+                <div className="flex flex-col">
+                    <div className="bg-color_bg bg-opacity-75 p-8 rounded-lg shadow-md flex flex-col md:flex-row items-center md:items-start mb-5 w-4/12" >
+                        <img className="w-32 rounded-full object-cover md:ml-4 mb-4 md:mb-0" src="/Public/Ellipse 56.png" alt="" />
+                        <div className="flex flex-col ml-4">
+                            <h1 className="font-bold p-2 ">Dr.Fulano Ciclano</h1>
+                            <p className="p-2 ">XX Anos</p>
+                            <p className="p-2 "> Nutricionista</p>
+                        </div>
+
+                    </div>
+
+                    <div className="bg-color_bg bg-opacity-75 p-8 rounded-lg shadow-md flex flex-col md:flex-row items-center md:items-start mb-5 w-4/12">
+
+                        <img className="w-32  rounded-full object-cover md:ml-4 mb-4 md:mb-0" src="/Public/Ellipse 56 (1).png" alt="" />
+                        <div className="flex flex-col ml-4">
+                            <h1 className="font-bold p-2 ">Fulano Ciclano</h1>
+                            <p className="p-2 ">XX Anos</p>
+                            <p className="p-2 "> Personal</p>
+                        </div>
+
+
+                    </div>
+
+
+                    <div className="bg-color_bg bg-opacity-75 p-8 rounded-lg shadow-md flex flex-col md:flex-row items-center md:items-start mb-5 w-4/12">
+
+                        <img className="w-32  rounded-full object-cover md:ml-4 mb-4 md:mb-0" src="/Public/Ellipse 57.png" alt="" />
+                        <div className="flex flex-col ml-4">
+                            <h1 className="font-bold p-2 ">Fulana Ciclana</h1>
+                            <p className="p-2 ">XX Anos</p>
+                            <p className="p-2 "> Psicóloga</p>
                         </div>
 
                     </div>
                 </div>
 
-                <h1 className="text-2xl text-color text-color_bg font-bold px-20 mt-24 ml-10 mb-32">Serviços</h1>
-                <div className=" px-8 mb-6 grid grid-cols-3 gap-8">
 
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-colortxt_white font-semibold mb-2">Estética Física</h1>
-                        <img className="w-56" src="/Public/faustoteste.png" />
-                    </div>
-
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-colortxt_white font-semibold mb-2">Saúde Corporal</h1>
-                        <img className="w-56" src="/Public/faustoteste.png" />
-                    </div>
-
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-colortxt_white font-semibold mb-2">Saúde Mental</h1>
-                        <img className="w-56" src="/Public/faustoteste.png" />
-                    </div>
-
-                    
-
+                <div className="bg-color_bg bg-opacity-75 p-8 rounded-lg w-8/12 h-full" >
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quis reprehenderit, officia recusandae architecto voluptatibus rem. Cupiditate temporibus quidem quos iure vero eos earum, optio deserunt, totam corrupti beatae doloribus!
+                    </p>
                 </div>
 
-                <div className="w-full flex flex-row justify-center mt-28 mb-24">
-                    <button type="submit" className="bg-color_bg text-color text-color_vinho py-2 rounded-xl w-60 mb-2 text-center font-semibold">Saber Mais</button>
-                </div>
-
-                <div className="card-container bg-color_bg bg-opacity-75 rounded-lg shadow-md">
-                    <h1 className="text-2xl text-color text-color_vinho font-bold pt-10 mt-24 ml-10">
-                        Planos Exclusivos
-                    </h1>
-                    <Planos/>
-                </div>
-                
             </div>
+        </div>
+
+
+
 
     )
 }
