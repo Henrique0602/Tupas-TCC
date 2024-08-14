f9680dd324fb578bf7a288e9815075a14363d5c1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 16:31:15 -0300

    Tupas-TCC

--- Tupa-s/src/Pages/Home.jsx
+++ Tupa-s/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
                 <Header />
                 <div className="bg-color_bg rounded-lg flex flex-row my-16 mx-16">
                     <div className="flex flex-col">
-                        <h2 className="text-color_vinho font-bold text-lg p-10">Bem - vindo</h2>
+                        <h2 className="text-color_vinho font-bold text-lg mt-2 ml-10 py-6">Bem - vindo</h2>
                         <h1 className="text-color_vinho font-bold text-4xl text-wrap max-w-sm ml-10 mb-4">Seu guia para o sucesso fisíco e mental.</h1>
                         <p className="text-color_vinho text-wrap max-w-72 ml-10"> 
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus provident corporis sapiente saepe,
